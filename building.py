eb8e19f954351197dbd75b1d4b696bb8a758ec03
Midhet <midhetfatema94@gmail.com>
2015-12-08 17:46:33 +0300

    Almost done

--- building.py
+++ building.py
@@ -1,25 +1,29 @@
 import random;
 
 def game():
-    tictactoe = [0,0,0,0,0,0,0,0,0];
-    
+    tictactoe = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+
     turn = who_goes_first();
-   
+
     for i in range(0,9):
         if(i%2==0):
-            position = input('\nPosition you want to play in: ');
-            tictactoe[position-1] = turn[0];
+            player_move(tictactoe, turn[0])
         else:
             print("\nComputer's Move: "+str(i+1))
             position = computer_move(tictactoe, turn[1], turn[0]);
             print('\n The position is: '+str(position));
             tictactoe[position] = turn[1]
-            
+
         draw_grid(tictactoe);
         if(i>=2):
             winner = is_winner(tictactoe);
             if(winner == 1):
-                break
+                choice = raw_input('Do you want to play another game? ');
+                choice.lower();
+                if(choice == 'yes'):
+                    game();
+                else:
+                    break
 
 def draw_grid(tictactoe):
 
@@ -29,65 +33,120 @@ def draw_grid(tictactoe):
     print('_|_|_')
     print(str(tictactoe[6])+'|'+ str(tictactoe[7])+'|'+ str(tictactoe[8]))
 
-def computer_move(ttt, mac_char, player_char):
+def player_move(ttt, char):
     
-    if(ttt[0]==ttt[2]==mac_char) or (ttt[0]==ttt[2]==player_char):
-        return 1;
-    elif(ttt[0]==ttt[1]==mac_char) or (ttt[0]==ttt[1]==player_char):
-        return 2;
-    elif(ttt[1]==ttt[2]==mac_char) or (ttt[1]==ttt[2]==player_char):
-        return 0;
-    elif(ttt[3]==ttt[4]==mac_char) or (ttt[3]==ttt[4]==player_char):
-        return 5;
-    elif(ttt[3]==ttt[5]==player_char) or (ttt[3]==ttt[5]==mac_char):
-        return 4;
-    elif(ttt[4]==ttt[5]==mac_char) or (ttt[4]==ttt[5]==player_char):
-        return 3;
-    elif(ttt[6]==ttt[7]==mac_char) or (ttt[6]==ttt[7]==player_char):
-        return 8;
-    elif(ttt[7]==ttt[8]==player_char) or (ttt[7]==ttt[8]==mac_char):
-        return 6;
-    elif(ttt[6]==ttt[8]==mac_char) or (ttt[6]==ttt[8]==player_char):
-        return 7;
-    elif(ttt[0]==ttt[3]==mac_char) or (ttt[0]==ttt[3]==player_char):
-        return 6;
-    elif(ttt[0]==ttt[6]==player_char) or (ttt[0]==ttt[6]==mac_char):
-        return 3;
-    elif(ttt[3]==ttt[6]==mac_char) or (ttt[3]==ttt[6]==player_char):
-        return 0;
-    elif(ttt[1]==ttt[4]==mac_char) or (ttt[1]==ttt[4]==player_char):
-        return 7;
-    elif(ttt[1]==ttt[7]==player_char) or (ttt[1]==ttt[7]==mac_char):
-        return 4;
-    elif(ttt[4]==ttt[7]==mac_char) or (ttt[4]==ttt[7]==player_char):
-        return 1;
-    elif(ttt[2]==ttt[5]==mac_char) or (ttt[2]==ttt[5]==player_char):
-        return 8;
-    elif(ttt[2]==ttt[8]==player_char) or (ttt[2]==ttt[8]==mac_char):
-        return 5;
-    elif(ttt[5]==ttt[8]==mac_char) or (ttt[5]==ttt[8]==player_char):
-        return 2;
-    elif(ttt[0]==ttt[4]==mac_char) or (ttt[0]==ttt[4]==player_char):
-        return 8;
-    elif(ttt[0]==ttt[8]==player_char) or (ttt[0]==ttt[8]==mac_char):
-        return 4;
-    elif(ttt[4]==ttt[8]==mac_char) or (ttt[4]==ttt[8]==player_char):
-        return 0;
-    elif(ttt[2]==ttt[4]==mac_char) or (ttt[2]==ttt[4]==player_char):
-        return 6;
-    elif(ttt[2]==ttt[6]==player_char) or (ttt[2]==ttt[6]==mac_char):
-        return 4;
-    elif(ttt[4]==ttt[6]==mac_char) or (ttt[4]==ttt[6]==player_char):
-        return 2;
+    position = input('\nPosition you want to play in: ');
+    if ttt[position-1] == ' ':
+        ttt[position-1] = char;
     else:
-        position = get_free_moves(ttt)
-        return random.choice(position)
+        print('Position is already occupied!');
+        player_move(ttt, char);
+                
+def computer_move(ttt, mac_char, player_char):
+
+    mwm = calculate_move(ttt, mac_char);
+    pwm = make_move(ttt, player_char);
+    if(mwm != []):
+        position = mwm;
+    else:
+        position = pwm;
+
+    print('\nPosition: '+str(position));
+    return position;
+
+def calculate_move(ttt, char):
+
+    position = [];
+    pos = [];
+    array = [];
+
+    if ttt[0]==ttt[2]==char:
+        pos.append(1);
+    if ttt[0]==ttt[1]==char:
+        pos.append(2);
+    if ttt[1]==ttt[2]==char:
+        pos.append(0);
+
+    if ttt[3]==ttt[4]==char:
+        pos.append(5);
+    if ttt[3]==ttt[5]==char:
+        pos.append(4);
+    if ttt[4]==ttt[5]==char:
+        pos.append(3);
+
+    if ttt[6]==ttt[7]==char:
+        pos.append(8);
+    if ttt[7]==ttt[8]==char:
+        pos.append(6);
+    if ttt[6]==ttt[8]==char:
+        pos.append(7);
+
+    if ttt[0]==ttt[3]==char:
+        pos.append(6);
+    if ttt[0]==ttt[6]==char:
+        pos.append(3);
+    if ttt[3]==ttt[6]==char:
+        pos.append(0);
+
+    if ttt[1]==ttt[4]==char:
+        pos.append(7);
+    if ttt[1]==ttt[7]==char:
+        pos.append(4);
+    if ttt[4]==ttt[7]==char:
+        pos.append(1);
+
+    if ttt[2]==ttt[5]==char:
+        pos.append(8);
+    if ttt[2]==ttt[8]==char:
+        pos.append(5);
+    if ttt[5]==ttt[8]==char:
+        pos.append(2);
+
+    if ttt[0]==ttt[4]==char:
+        pos.append(8);
+    if ttt[0]==ttt[8]==char:
+        pos.append(4);
+    if ttt[4]==ttt[8]==char:
+        pos.append(0);
+
+    if ttt[2]==ttt[4]==char:
+        pos.append(6);
+    if ttt[2]==ttt[6]==char:
+        pos.append(4);
+    if ttt[4]==ttt[6]==char:
+        pos.append(2);
+
+        print('\nPoses: '+str(pos));
+        for i in range(len(pos)):
+            if ttt[pos[i]] == ' ':
+                array.append(pos[i])
+
+        position = random.choice(array);
+
+    print('\nMachine winning move: '+str(position));
+    return position;
+
+
+def make_move(ttt, player_char):
+    pos = [];
+    position = [];
+    pos = calculate_move(ttt, player_char);
+
+    if(pos != []):
+        position = pos;
+    else:
+        positions = get_free_moves(ttt);
+        position = random.choice(positions);
+
+    print("\nPlayer's winning move: "+str(position));
+    return position;
+
     
 def get_free_moves(ttt):
 
     array = [];
     for i in range(0,9):
-        if(ttt[i]==0):
+        if(ttt[i]==' '):
             array.append(i);
     return array;
 
@@ -102,7 +161,7 @@ def who_goes_first():
     else:
         one = 'O'
         two = 'X'
-        
+
     return one, two;
 
 def is_winner(ttt):
@@ -116,7 +175,7 @@ def is_winner(ttt):
        (ttt[2]==ttt[4]==ttt[6]=='X')):
         print('X is the winner!');
         return 1
-        
+
 
     elif ((ttt[0]==ttt[1]==ttt[2]=='O') or
        (ttt[3]==ttt[4]==ttt[5]=='O') or
@@ -130,5 +189,5 @@ def is_winner(ttt):
         return 1
 
     return 0
-        
+
 game()
